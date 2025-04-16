9a654df9084e8781c47dc29d7333b6ad1fd01895
Joe Higgs <147166151+nonebula@users.noreply.github.com>
2025-04-16 11:55:19 +0100

    TGP-3377 - update gatling-sbt version (#33)
    * TGP-3377 - update gatling-sbt version
    
    * TGP-3377 - update other dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,10 +5,10 @@ object Dependencies {
   private val gatlingVersion = "3.6.1"
 
   private val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.1.0",
-    "org.mongodb.scala"    %% "mongo-scala-driver"        % "4.9.0",
-    "org.scalacheck"       %% "scalacheck"                % "1.17.0",
-    "com.typesafe"          % "config"                    % "1.4.2"
+    "uk.gov.hmrc"       %% "performance-test-runner" % "6.1.0",
+    "org.mongodb.scala" %% "mongo-scala-driver"      % "4.9.0",
+    "org.scalacheck"    %% "scalacheck"              % "1.17.0",
+    "com.typesafe"       % "config"                  % "1.4.2"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = test
